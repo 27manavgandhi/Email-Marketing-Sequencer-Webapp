861a64fc7d70d452ca7538ad60c6c881e6f5e54c
Manav Gandhi <27manavgandhi@gmail.com>
2024-07-03 14:18:13 +0530

    Update mongoDBCon.js

--- backend/src/config/mongoDBCon.js
+++ backend/src/config/mongoDBCon.js
@@ -1,13 +1,7 @@
 import mongoose from "mongoose";
 import configEnv from "./configEnv.js";
 
+
 export const mongoDBCon = async () => {
-    console.log('Attempting to connect to MongoDB...');
-    try {
-        await mongoose.connect(configEnv.db);
-        console.log('Successfully connected to MongoDB');
-    } catch (error) {
-        console.error('Failed to connect to MongoDB:', error);
-        throw error; // Re-throw the error so it can be caught by the caller
-    }
+    return mongoose.connect(configEnv.db)
 }
