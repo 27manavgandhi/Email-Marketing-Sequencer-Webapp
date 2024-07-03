7be4a7826ffbd5e1f37e1a87ca06aee409f9d78f
Manav Gandhi <27manavgandhi@gmail.com>
2024-07-03 13:47:02 +0530

    Update configEnv.js

--- backend/src/config/configEnv.js
+++ backend/src/config/configEnv.js
@@ -1,21 +1,11 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-const env = process.env.NODE_ENV; // development , production
+const env = process.env.NODE_ENV || 'development'; // default to development if not set
 
-let configEnv = null;
-
-if (env === 'development') {
-    configEnv = {
-        db: process.env.MONGODB_URL,
-        port: process.env.PORT
-    }
-}
-else {
-    configEnv = {
-        db: 'mongodb://127.0.0.1:27017/futureBlinkDB',
-        port: process.env.PORT
-    }
-}
+const configEnv = {
+    db: process.env.MONGODB_URL,
+    port: process.env.PORT
+};
 
 export default configEnv;
